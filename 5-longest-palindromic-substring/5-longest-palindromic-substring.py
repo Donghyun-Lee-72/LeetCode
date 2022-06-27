260cbf6ed43be016ad61e1cb4c4dc6416ae5b186
Donghyun Lee <49599355+Donghyun-Lee-72@users.noreply.github.com>
2022-06-27 12:20:44 +0900

    Time: 164 ms (97.86%), Space: 14.1 MB (29.91%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,12 +1,11 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        s_rev = s[::-1]                             # reversed input string
-        for length in range(len(s), 0, -1):         # from full length to 2
-            last_start = len(s) - length + 1        # last starting idx at given length
-            for start in range(0, last_start):      # idx of first element in substring
-                half = int(round(length/2))         # half of length, rounded up
-                end = len(s) - length - start       # rev_idx of last element in substring
-                if s[start:start+half] == s_rev[end:end+half]:
-                    substr = s[start:start+length]
-                    return substr
-                
+        if len(s) <= 1:
+            return s
+        i,l=0,0
+        for j in range(len(s)):
+            if s[j-l: j+1] == s[j-l: j+1][::-1]:
+                i, l = j-l, l+1
+            elif j-l > 0 and s[j-l-1: j+1] == s[j-l-1: j+1][::-1]:
+                i, l = j-l-1, l+2
+        return s[i: i+l]
