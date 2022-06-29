95e071b34e3de4963823f55a42bcd9f11ecc47df
Donghyun Lee <49599355+Donghyun-Lee-72@users.noreply.github.com>
2022-06-29 23:32:51 +0900

    Time: 82 ms (35.47%), Space: 14 MB (30.41%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,19 +1,15 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         roman = { "I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000 }
+        roman_set = { "IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD":400, "CM":900 }
         num = 0             # total number added
-        sub = 0             # if roman was subtraction, use 'sub' to subtract
-        for char in s:    
-            # if subtraction was started in last digit
-            if sub != 0 and sub < roman[char] < sub*11:
-                num = num - 2*sub
-                sub = 0
-            
-            # if subtraction can be started
-            if char in ["I", "X", "C"]:
-                sub = roman[char]
-            
-            # add every value
-            num += roman[char]
-            
+        i = 0
+        while i < len(s):
+            if s[i:i+2] in roman_set:
+                num += roman_set[s[i:i+2]]
+                i += 2
+            else:
+                num += roman[s[i]]
+                i += 1
+                
         return num
