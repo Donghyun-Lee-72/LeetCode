272d0b07d3cd74c966cf429f07ae08a226e680ec
Donghyun Lee <49599355+Donghyun-Lee-72@users.noreply.github.com>
2022-07-13 22:28:22 +0900

    Time: 34 ms (91.64%), Space: 13.9 MB (82.60%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        arr = [[0]*n for i in range(n)]
+        i = 0
+        next_stop = n
+        x, y = 0, 0
+        cnt = 1
+        
+        while cnt <= n*n:
+            arr[y][x] = cnt
+            if cnt == next_stop:
+                i += 1
+                next_stop += n - (i+1)//2
+                
+            cnt += 1
+            
+            if i % 4 == 0:
+                x += 1
+            elif i % 4 == 1:
+                y += 1
+            elif i % 4 == 2:
+                x -= 1
+            else:
+                y -= 1
+                
+        return arr
