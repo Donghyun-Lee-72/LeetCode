387bb6fd5d2d9770bfdab0197f5ed07667505753
Donghyun Lee <49599355+Donghyun-Lee-72@users.noreply.github.com>
2022-09-27 01:34:43 -0500

    Time: 351 ms (21.68%), Space: 32.2 MB (8.74%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -3,13 +3,10 @@ class Solution:
         import numpy as np
         
         satisfaction_sorted = np.sort(satisfaction)
-        multiplier = np.arange(1,len(satisfaction)+1)
-        coef = np.sum(satisfaction_sorted * multiplier)
-        
         for i in range(len(satisfaction)):
             sum = np.sum(satisfaction_sorted[i:])
             if 0 < sum:
-                return coef
-            else:
-                coef -= sum
-        return coef
+                break
+        
+        coef = np.sum(satisfaction_sorted[i:]*np.arange(1,len(satisfaction_sorted)+1-i))
+        return max(0, coef)
