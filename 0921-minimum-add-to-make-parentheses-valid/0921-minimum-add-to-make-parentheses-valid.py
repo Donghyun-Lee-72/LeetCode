21fa178a3de5ec2a6529a5f644a994dbdcedef6c
Donghyun Lee <dhlee991124@gmail.com>
2022-10-16 03:04:05 -0500

    Time: 47 ms (68.44%), Space: 13.8 MB (57.69%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -4,9 +4,8 @@ class Solution:
         for char in s:
             if char == "(":
                 cnt[0] += 1
+            elif cnt[0] > 0:
+                cnt[0] -= 1
             else:
-                if cnt[0] > 0:
-                    cnt[0] -= 1
-                else:
-                    cnt[1] += 1
+                cnt[1] += 1
         return sum(cnt)
